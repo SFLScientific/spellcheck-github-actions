2714b1ebd5e5497e59ccc3824f4cb5a23464c6f5
Darryn Ten <darryn.ten@gmail.com>
2020-02-27 14:43:33 +0700

    Modify the default config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if [ ! -f ./spellcheck.yaml ]; then
+    ls -la
     echo "spellcheck.yaml not found, using default\n"
     cp /spellcheck.yaml .
 fi
 
 if [ ! -f ./wordlist.txt ]; then
+    ls -la
     echo "wordlist.txt not found, using default\n"
     cp /wordlist.txt .
 fi
