f489ae9259249a19bb18a9c044579cad0b2d2046
Darryn Ten <darryn.ten@gmail.com>
2020-02-27 14:49:32 +0700

    Change path for overriding yml file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ ! -f ./spellcheck.yaml ]; then
+if [ ! -f spellcheck.yaml ]; then
     ls -la
     echo "spellcheck.yaml not found, using default\n"
     cp /spellcheck.yaml .
 fi
 
-if [ ! -f ./wordlist.txt ]; then
+if [ ! -f wordlist.txt ]; then
     ls -la
     echo "wordlist.txt not found, using default\n"
     cp /wordlist.txt .
